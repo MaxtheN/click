fb884bdef4e9b53070fcc3a34d4755bbc7c3b81a
TheN <max.nurislom@gmail.com>
2023-08-03 19:56:15 +0500

    a lot of reforms and fixes mainly due to the deprecated methods in dependencies

--- click/views.py
+++ click/views.py
@@ -3,15 +3,17 @@ from django.views.decorators.csrf import csrf_exempt
 from . import utils
 from . import Services
 
+
 @csrf_exempt
 def prepare(request):
     return utils.prepare(request)
+
 
 @csrf_exempt
 def complete(request):
     return utils.complete(request)
 
+
 @csrf_exempt
 def service(request, service_type):
-    service = Services(request.POST, service_type)
-    return JsonResponse(service.api())
+    return JsonResponse(Services(request.POST, service_type).api())
